fb4c6677fb9e35e30e36ca2e4150eb98e836a940
Gregory Evgrafov <evgrafovgregory@gmail.com>
2025-11-07 16:18:33 +0100

    add HTML node classes and converter function from TextNode

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -1,8 +1,8 @@
 class HTMLnode:
-    def __init__(self, tag = None, value = None, childern = None, props = None):
+    def __init__(self, tag = None, value = None, children = None, props = None):
         self.tag = tag
         self.value = value
-        self.children = childern
+        self.children = children
         self.props = props
     def to_html(self):
         raise NotImplementedError
@@ -15,3 +15,34 @@ class HTMLnode:
         return result
     def __repr__(self):
         return f"HTMLnode({self.tag}, {self.value}, {self.children}, {self.props})"
+
+class LeafNode(HTMLnode):
+    def __init__(self, tag, value, props = None):
+        super().__init__(tag=tag, value=value, props=props)
+
+    def to_html(self):
+        if self.value is None:
+            raise ValueError
+        if self.tag is None:
+            return self.value
+        opening_tag = f"<{self.tag}>"
+        closing_tag = f"</{self.tag}>"
+        element = opening_tag + self.value + closing_tag
+        return element
+
+class ParentNode(HTMLnode):
+    def __init__(self, tag, children, props = None):
+        super().__init__(tag=tag,children=children,props=props)
+    def to_html(self):
+        if self.tag is None:
+            raise ValueError("No tag provided")
+        if self.children is None:
+            raise ValueError("Children elemetns are missing!")
+
+        opening_tag = f"<{self.tag}>"
+        closing_tag = f"</{self.tag}>"
+        element = opening_tag
+        for child in self.children:
+            element += child.to_html()
+        element += closing_tag
+        return element
--- src/textnode.py
+++ src/textnode.py
@@ -1,4 +1,6 @@
 from enum import Enum
+from htmlnode import LeafNode
+
 class TextType(Enum):
 
     PLAIN_TEXT = "text (plain)"
@@ -21,3 +23,21 @@ class TextNode():
 
     def __repr__(self):
         return f"TextNode({self.text}, {self.text_type.value}, {self.url})"
+
+
+def text_node_to_html_node(text_node):
+    match text_node.text_type:
+        case TextType.PLAIN_TEXT:
+            return LeafNode(tag = None, value = text_node.text)
+        case TextType.BOLD_TEXT:
+            return LeafNode(tag="b", value=text_node.text)
+        case TextType.ITALIC_TEXT:
+            return LeafNode(tag="i", value=text_node.text)
+        case TextType.CODE_TEXT:
+            return LeafNode(tag="code", value=text_node.text)
+        case TextType.LINKS:
+            return LeafNode(tag="a", value=text_node.text, props={"href":text_node.url})
+        case TextType.IMAGES:
+            return LeafNode(tag="img", value="", props={"src":text_node.url, "alt":text_node.text})
+        case _:
+            raise Exception("Unknown text node type")
